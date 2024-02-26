675baaaf0d6bcc4e1d45cf0682e18aff72be775f
liuseen-l <773890440@qq.com>
2024-02-27 00:02:07 +0800

    chore:eslint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -16,9 +16,30 @@ const scopes = [
 
 export default {
   rules: {
+    /**
+     * type[scope]: [function] description
+     *      ^^^^^
+     */
     'scope-enum': [2, 'always', scopes],
+    /**
+     * type[scope]: [function] description
+     *
+     * ^^^^^^^^^^^^^^ empty line.
+     * - Something here
+     */
     'body-leading-blank': [1, 'always'],
+    /**
+     * type[scope]: [function] description
+     *
+     * - something here
+     *
+     * ^^^^^^^^^^^^^^
+     */
     'footer-leading-blank': [1, 'always'],
+    /**
+     * type[scope]: [function] description [No more than 72 characters]
+     *      ^^^^^
+     */
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-case': [
@@ -30,6 +51,10 @@ export default {
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
+    /**
+     * type[scope]: [function] description
+     * ^^^^
+     */
     'type-enum': [
       2,
       'always',
