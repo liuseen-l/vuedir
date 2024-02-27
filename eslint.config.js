b6de134931d286831d7a2e4493c026089d1e3126
liujiahao <liujiahao.118@bytedance.com>
2024-02-27 20:10:01 +0800

    chore: lint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    typescript: true,
+    vue: true,
+    jsonc: true,
+    yaml: false,
+    markdown: false
+  }, {
+}
+)
