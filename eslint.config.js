c242aae4859861fce40ceccf64dd93858bbb7d17
liuseen-l <773890440@qq.com>
2024-02-27 20:57:34 +0800

    chore: config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,12 @@ export default antfu(
     typescript: true,
     vue: true,
     jsonc: true,
+    jsx: false,
     yaml: false,
     markdown: false,
+    ignores: [
+      ' **/dist',
+      '**/node_modules',
+    ],
   },
 )
